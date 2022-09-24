dce6c587a4c4369c34485f5ff30801165744e78f
py-e <gitpye@gmail.com>
2022-09-25 00:31:01 +0300

    Sorting and print moved to the function

--- count_words.py
+++ count_words.py
@@ -11,6 +11,13 @@ def cleanup_word(word):
     return word[:-1] if word[-1] in (',', '.', '?', '!') else word
 
 
+def print_sorted_by_number(words):
+    sorted_words = {k: v for k, v in sorted(words.items(), key=lambda item: item[1][0], reverse=True)}
+    print(f'Total number of detected words: {len(sorted_words)}')
+    for w in sorted_words:
+        print(w, sorted_words[w][0], sorted_words[w][1])
+
+
 def count_words(text):
     splitted_text = text.split()
     words_counter = {}
@@ -21,14 +28,12 @@ def count_words(text):
         else:
             word_frequency = TOP100 if w in top_100_english_words else ''
             words_counter[w] = [1, word_frequency]
-    sorted_words = {k: v for k, v in sorted(words_counter.items(), key=lambda item: item[1][0], reverse=True)}
-    print(f'Total number of detected words: {len(sorted_words)}')
-    for w in sorted_words:
-        print(w, sorted_words[w][0], sorted_words[w][1])
+    return words_counter
 
 
 def main():
-    count_words(insert_text())
+    words = count_words(insert_text())
+    print_sorted_by_number(words)
 
 
 if __name__ == '__main__':
