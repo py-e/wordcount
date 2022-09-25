862456ec708006b01a9c0cd84e7afdaff4241e23
py-e <gitpye@gmail.com>
2022-09-26 00:41:47 +0300

    Update of count & cleanup

--- count_words.py
+++ count_words.py
@@ -2,13 +2,18 @@ from words_dicts import top_100_english_words
 
 TOP100 = '(in top 100)'
 
+
 def insert_text():
     t = input('Provide the text: ')
     return t
 
 
 def cleanup_word(word):
-    return word[:-1] if word[-1] in (',', '.', '?', '!') else word
+    """Remove first/last symbol if it's not a letter.
+    ex.: '(robots' -> 'robots'; 'robots:' -> 'robots'."""
+    word = word[1:] if not word[0].islower() else word
+    word = word[:-1] if not word[-1].islower() else word
+    return word
 
 
 def print_sorted_by_number(words):
@@ -23,20 +28,27 @@ def print_sorted_by_number(words):
 
 
 def count_words(text):
-    text_words = (cleanup_word(w).lower() for w in text.split())
+    # text_words = (cleanup_word(w).lower() for w in text.split())
     words_counter = {}
-    for w in text_words:
-        if w in words_counter:
-            words_counter[w][0] += 1
+    not_words = set()
+    for w in text.split():
+        w = w.lower()
+        if w.islower():
+            w = cleanup_word(w)
+            if w in words_counter:
+                words_counter[w][0] += 1
+            else:
+                word_frequency = TOP100 if w in top_100_english_words else ''
+                words_counter[w] = [1, word_frequency]
         else:
-            word_frequency = TOP100 if w in top_100_english_words else ''
-            words_counter[w] = [1, word_frequency]
-    return words_counter
+            not_words.add(w)
+    return words_counter, not_words
 
 
 def main():
-    words = count_words(insert_text())
+    words, not_words = count_words(insert_text())
     print_sorted_by_number(words)
+    print(f'Items excluded (not words): {not_words}')
 
 
 if __name__ == '__main__':
