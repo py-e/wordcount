b5dd7bab25f5b9f64d4b8d5b395928a9735af71e
py-e <gitpye@gmail.com>
2022-09-26 13:51:34 +0300

    Refactoring: helper functions

--- count_words.py
+++ count_words.py
@@ -16,10 +16,12 @@ def cleanup_word(word):
     return word
 
 
-def print_sorted_by_number(words):
+def print_totals(words):
     print(f'Total number of the words: {sum(words[w][0] for w in words)}')
     print(f'Number of the unique words: {len(words)}')
 
+
+def print_sorted_by_number(words):
     sorted_words = {k: v for k, v in sorted(words.items(), key=lambda item: item[1][0], reverse=True)}
     for w in sorted_words:
         print(f'{sorted_words[w][0]:<5}', end='')
@@ -27,19 +29,22 @@ def print_sorted_by_number(words):
         print(sorted_words[w][1])
 
 
+def add_to_counter(w, words_counter):
+    if w in words_counter:
+        words_counter[w][0] += 1
+    else:
+        word_frequency = TOP100 if w in top_100_english_words else ''
+        words_counter[w] = [1, word_frequency]
+
+
 def count_words(text):
-    # text_words = (cleanup_word(w).lower() for w in text.split())
     words_counter = {}
     not_words = set()
     for w in text.split():
         w = w.lower()
         if w.islower():
             w = cleanup_word(w)
-            if w in words_counter:
-                words_counter[w][0] += 1
-            else:
-                word_frequency = TOP100 if w in top_100_english_words else ''
-                words_counter[w] = [1, word_frequency]
+            add_to_counter(w, words_counter)
         else:
             not_words.add(w)
     return words_counter, not_words
@@ -47,6 +52,7 @@ def count_words(text):
 
 def main():
     words, not_words = count_words(insert_text())
+    print_totals(words)
     print_sorted_by_number(words)
     print(f'Items excluded (not words): {not_words}')
 
