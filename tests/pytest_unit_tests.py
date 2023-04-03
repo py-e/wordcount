7ac4ce60e980f074b5bf8ff7919db77ab130b38f
py-e <gitpye@gmail.com>
2023-04-04 02:24:29 +0300

    Unittests: conversion to pytest started

---
+++ tests/pytest_unit_tests.py
@@ -0,0 +1,193 @@
+import os
+import sys
+import pytest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import count_words
+
+
+p_cleanup_beginning = (
+    pytest.param('-15:recursion', 'recursion',      id='symbol_at_the_beginning'),
+    pytest.param('recursion', 'recursion',          id='clean_word'),
+    pytest.param('recur-sion', 'recur-sion',        id='symbol_inside'),
+    pytest.param('recursion-#7;', 'recursion-#7;',  id='symbol_at_the_end'))
+@pytest.mark.parametrize('send, exp', p_cleanup_beginning)
+def test_cleanup_beginning(send, exp):
+    """count_words.cleanup_beginning(word)"""
+    ret = count_words.cleanup_beginning(send)
+    assert exp == ret
+
+
+p_cleanup_end = (
+    pytest.param('recursion-#7;', 'recursion',      id='symbol_at_the_end'),
+    pytest.param('recursion', 'recursion',          id='clean_word'),
+    pytest.param('recur-sion', 'recur-sion',        id='symbol_inside'),
+    pytest.param('-15:recursion;', '-15:recursion', id='symbol_at_the_beginning'))
+@pytest.mark.parametrize('send, exp', p_cleanup_end)
+def test_cleanup_end(send, exp):
+    """count_words.cleanup_beginning(word)"""
+    ret = count_words.cleanup_end(send)
+    assert exp == ret
+
+
+@pytest.mark.parametrize('send, exp', (('-15:recursion-#7;', 'recursion'),), ids=('symbols_at_both_sides',))
+def test_cleanup_word(send, exp):
+    """count_words.cleanup_beginning(word)
+    Sort of integration test: launch other modules (cleanup_beginning, cleanup_end)"""
+    ret = count_words.cleanup_word(send)
+    assert exp == ret
+
+
+id1 = 'sort_only'
+inp1 = {'one':     [1, '(in top 100)', '', ''],
+        'ten':     [10, '(from 100 to 1000)', '', ''],
+        'fifteen': [15, '', '(1 with s: fifteens)', ''],
+        'two':     [2, '(in top 100)', '', ''],
+        'eighty':  [80, '(l1)', '', ''],
+        'twice':   [2, '(from 100 to 1000)', '', '']}
+exc1 = None
+out1 = {'one':     [1, '(in top 100)', '', 6],
+        'ten':     [10, '(from 100 to 1000)', '', 3],
+        'fifteen': [15, '', '(1 with s: fifteens)', 2],
+        'two':     [2, '(in top 100)', '', 4],
+        'eighty':  [80, '(l1)', '', 1],
+        'twice':   [2, '(from 100 to 1000)', '', 5]}
+
+id2 = 'sort_and_exclude'
+inp2 = inp1
+exc2 = ['(in top 100)', '(l1)']
+out2 = {'ten':     [10, '(from 100 to 1000)', '', 2],
+        'fifteen': [15, '', '(1 with s: fifteens)', 1],
+        'twice':   [2, '(from 100 to 1000)', '', 3]}
+
+id3 = 'sort_only_empty'
+inp3 = {}
+exc3 = None
+out3 = {}
+
+id4 = 'sort_and_exclude_empty'
+inp4 = {}
+exc4 = ['(in top 100)', '(l1)']
+out4 = {}
+
+id5 = 'sort_only_wrong_type_int_instead_str'
+inp5 = {1: [1, '', '', '']}
+exc5 = None
+out5 = inp5
+
+id6 = 'wrong_exclude_value'
+inp6 = {'word': [1, '', '', ''], 'and': [5, '', '', '']}
+exc6 = ['(no such value)']
+out6 = {'word': [1, '', '', 2], 'and': [5, '', '', 1]}
+
+@pytest.mark.parametrize('inp, exc, out',
+                         ((inp1, exc1, out1), (inp2, exc2, out2), (inp3, exc3, out3),
+                          (inp4, exc4, out4), (inp5, exc5, out5), (inp6, exc6, out6)),
+                         ids=(id1, id2, id3, id4, id5, id6))
+def test_dataSortAndExclude(inp, exc, out):
+    """count_words.sort_and_exclude(words, exclude=None)"""
+    if exc:
+        ret_sorted_words = count_words.sort_and_exclude(inp, exc)
+    else:
+        ret_sorted_words = count_words.sort_and_exclude(inp)
+    assert out == ret_sorted_words
+
+id7 = 'sort_only_wrong_type_int_instead_list'
+inp7 = {'word': 1}
+exc7 = None
+out7 = "'int' object is not subscriptable"
+
+id8 = 'wrong_signature'
+inp8 = {'word': [1, '', '', '']}
+exc8 = (['(in top 100)', '(l1)'], 'unexpected value')
+out8 = 'takes from 1 to 2 positional arguments but 3 were given'
+
+@pytest.mark.parametrize('inp, exc, out', ((inp7, exc7, out7), (inp8, exc8, out8)), ids=(id7, id8))
+def test_dataSortAndExclude_exception(inp, exc, out):
+    """count_words.sort_and_exclude(words, exclude=None)"""
+    with pytest.raises(TypeError) as excinfo:
+        if exc:
+            ret_sorted_words = count_words.sort_and_exclude(inp, exc[0], exc[1])
+        else:
+            ret_sorted_words = count_words.sort_and_exclude(inp)
+    assert out in str(excinfo)
+
+
+@pytest.mark.parametrize(
+'word, l1, l2, words_counter, num,                                                          exp',
+
+(('word', ['word', 'test', 'debug'], ['prerequisite', 'snippet'], {}, 1,                    {'word': [1, '(from 100 to 1000)', '', '']}),
+('word', ['word', 'test', 'debug'], ['prerequisite', 'snippet'], {}, 2,                     {'word': [2, '(from 100 to 1000)', '', '']}),
+('wordnotinlist', ['wordnotinlist', 'test', 'debug'], ['prerequisite', 'snippet'], {}, 1,   {'wordnotinlist': [1, '(l1)', '', '']}),
+('wordnotinlist', ['test', 'debug'], ['prerequisite', 'snippet'], {}, 1,                    {'wordnotinlist': [1, '', '', '']})),
+
+ids=('from_top1000_once', 'from_top1000_twice', 'once_l1', 'once'))
+def test_countWord(word, l1, l2, words_counter, num, exp):
+    """count_words.add_to_counter(w, words_counter, l1, l2)"""
+    for i in range(num):
+        count_words.add_to_counter(word, words_counter, l1, l2)
+    assert exp == words_counter
+
+
+@pytest.mark.parametrize('words, exp',
+(({'moss': [1, '', '', ''], "moss's": [1, '', '', '']},     {'moss': [2, '', "(1 with 's: moss's)", '']}),),
+                         ids=('checking_s',))
+def test_count_apostrophes(words, exp):
+    """count_words.count_apostrophes(words)
+    Kind of integration test: count_apostrophes launches recount_variants"""
+    count_words.count_apostrophes(words)
+    assert exp == words
+
+
+@pytest.mark.parametrize('words, exp',
+
+(({'word': [1, '(from 100 to 1000)', '', ''], 'words': [1, '', '', ''], 'worded': [1, '', '', ''], 'wording': [1, '', '', '']},
+ {'word': [4, '(from 100 to 1000)', '(1 with s: words)(1 with ed: worded)(1 with ing: wording)', '']}),
+
+({'moss': [1, '', '', ''], 'mosses': [1, '', '', '']},
+ {'moss': [2, '', '(1 with es: mosses)', '']}),
+
+({'observe': [1, '', '', ''], 'observed': [1, '', '', '']},
+ {'observe': [2, '', '(1 with d: observed)', '']})),
+
+                         ids=('_s_ed_ing', '_es', '_d'))
+def test_count_forms(words, exp):
+    """count_words.count_forms(words)
+    Kind of integration test: count_forms launches recount_variants
+    Checking endings: 's', 'es', 'ed', 'd', 'ing'
+    """
+    count_words.count_forms(words)
+    assert exp == words
+
+
+@pytest.mark.parametrize('words, exp',
+
+(({'a': [1, '(in top 100)', '', 1]},
+ {1: [['a'], 1]}),
+
+({'dot': [2, '', '', 1], 'ten': [1, '(from 100 to 1000)', '', 2]},
+ {3: [['dot', 'ten'], 2]}),
+
+({'a': [1, '(in top 100)', '', 1], 'objectification': [1, '', '', 2]},
+ {1: [['a'], 1], 15: [['objectification'], 1]})),
+
+                         ids=('1_word_1_symbol', '2_words_3_symbols_each', '2_words_1_and_15_symbols'))
+def test_sizeOfWords(words, exp):
+    """
+    count_words.size_of_words(words)
+    Simple test: one word counted
+    """
+    ret_sizes = count_words.size_of_words(words)
+    assert exp == ret_sizes
+
+
+@pytest.mark.parametrize('index, list_words, to_add, exp',
+(({2},  ['one', 'two', 'three'], set(),             {'two'}),
+({2, 3},['one', 'two', 'three'], set(),             {'two', 'three'}),
+({0},   ['one', 'two', 'three'], set(),             set()),
+({0.5}, ['one', 'two', 'three'], set(),             set())),
+                         ids=('one_word', 'two_words', 'index_out_of_range', 'index_not_int'))
+def test_findWordInDataStructureByIndex(index, list_words, to_add, exp):
+    """count_words.get_words_by_indexes(indexes_to_add, list_words, words_to_add)"""
+    count_words.get_words_by_indexes(index, list_words, to_add)
+    assert exp == to_add
