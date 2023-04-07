c1bb61d4ef1b246d416abf82883625972a6cff5f
py-e <gitpye@gmail.com>
2023-04-07 18:42:51 +0300

    Move parameters from fixtures to tests

--- tests/integration-tests/pytest_integration_tests.py
+++ tests/integration-tests/pytest_integration_tests.py
@@ -6,13 +6,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 import count_words
 
 
-def test_count_words(text_files_expected):
-    text, expected, not_words = text_files_expected
-    ret_words_counter, ret_not_words = count_words.count_words(text)
-    assert expected == ret_words_counter
-    assert set(not_words) == ret_not_words
-
-
 # text for SUT function: 4, 3, 2 word variants; one word in l2; two not words,
 p_word4 = {'word': 'word', 'variants': ('word', '-&7word', 'word")', 'word")')}
 p_word3 = {'word': 'test', 'variants': ('-=test=-', 'test....', r'\test/')}
@@ -26,15 +19,24 @@ p_files = ([p_word2['word']],)
 p_expected = {p_word3['word']: [3, '(from 100 to 1000)', '', ''],
               p_word4['word']: [4, '(from 100 to 1000)', '', ''],
               p_word2['word']: [2, '(l2)', '', '']}
-@pytest.fixture(params=[(p_text, p_base, p_files, p_expected, p_not_words)], ids=['text432not'])
-def text_files_expected(tmp_SUT_path, request):
-    text, base, files, expected, not_words = request.param
-    create_txt(tmp_SUT_path, base, files)
-    yield text, expected, not_words
+@pytest.mark.parametrize('text_base_files_expected_notwords', [(p_text, p_base, p_files, p_expected, p_not_words)],
+                    ids=['text432not'],
+                    indirect=['text_base_files_expected_notwords'])
+def test_count_words(text_base_files_expected_notwords):
+    text, expected, not_words = text_base_files_expected_notwords
+    ret_words_counter, ret_not_words = count_words.count_words(text)
+    assert expected == ret_words_counter
+    assert set(not_words) == ret_not_words
 
 
-def test_get_sorted_list_from_base(files_first_expected):
-    base, first, exp = files_first_expected
+@pytest.mark.parametrize('base_files_first_expected', [
+('l1', (['hello', 'hi'], ['goodbye'], ['bye']), None, None),    # expected None will be updated (to all words from files)
+('l1', (['hello', 'hi'], ['goodbye'], ['bye']), 'he', ['hello']),
+('l1', (['hello', 'hi'], ['goodbye'], ['bye']), 'n',  [])],     # new case (absent in unittest_integration_tests): file is not found for first letter(s)
+                ids=['l1_hgb-all', 'l1_hgb-he_found', 'l1_hgb-n_not_found'],
+                indirect=['base_files_first_expected'])
+def test_get_sorted_list_from_base(base_files_first_expected):
+    base, first, exp = base_files_first_expected
     if first is None:
         sorted_words_from_base = count_words.get_sorted_list_from_base(base)
     else:
@@ -42,19 +44,7 @@ def test_get_sorted_list_from_base(files_first_expected):
     assert sorted(exp) == sorted_words_from_base
 
 
-@pytest.fixture(params=[
-('l1', (['hello', 'hi'], ['goodbye'], ['bye']), None, None),
-('l1', (['hello', 'hi'], ['goodbye'], ['bye']), 'he', ['hello']),
-('l1', (['hello', 'hi'], ['goodbye'], ['bye']), 'n',  [])],   # new case (absent in unittest_integration_tests), file is not found for first letter(s)
-                ids=['l1_hgb-all', 'l1_hgb-he_found', 'l1_hgb-n_not_found'])
-def files_first_expected(tmp_SUT_path, request):
-    base, files, first, exp = request.param
-    all_words, _ = create_txt(tmp_SUT_path, base, files)
-    if exp is None:
-        exp = all_words
-    yield base, first, exp
-
-
+@pytest.mark.parametrize('base_word', [('l1', 'hello')], ids=['l1_h'], indirect=['base_word'])
 def test_write_read_base(base_word):
     base_dir, base, word_for_test = base_word
     count_words.write_to_base(base, word_for_test)
@@ -63,12 +53,30 @@ def test_write_read_base(base_word):
     assert ret is True
 
 
-@pytest.fixture(params=[('l1', 'hello')], ids=['l1_h'])
+@pytest.fixture
 def base_word(tmp_SUT_path, request):
     base, word_for_test = request.param
     base_dir = tmp_SUT_path / base
     base_dir.mkdir()
     yield base_dir, base, word_for_test
+
+
+### Fixtures
+
+@pytest.fixture
+def text_base_files_expected_notwords(tmp_SUT_path, request):
+    text, base, files, expected, not_words = request.param
+    create_txt(tmp_SUT_path, base, files)
+    yield text, expected, not_words
+
+
+@pytest.fixture
+def base_files_first_expected(tmp_SUT_path, request):
+    base, files, first, exp = request.param
+    all_words, _ = create_txt(tmp_SUT_path, base, files)
+    if exp is None:
+        exp = all_words
+    yield base, first, exp
 
 
 ### Utils
@@ -95,6 +103,7 @@ def create_txt(tmp_SUT_path, base, files):
                 all_words.append(word)
                 f.write(word + '\n')
     return all_words, file_paths
+
 
 def file_content(base_dir, word):
     base_file = word[0]+'.txt'
