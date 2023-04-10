df5f0bbe0dbf6642110d00527981d77f77f045c4
py-e <gitpye@gmail.com>
2023-04-10 18:53:07 +0300

    Custom exception: csv is not found

--- tests/hook_csv_parameters/conftest.py
+++ tests/hook_csv_parameters/conftest.py
@@ -11,25 +11,32 @@ def pytest_generate_tests(metafunc):
 
 
 def get_data_from_csv(parameters_file):
-    indirect_row = pd.read_csv(parameters_file, sep=';', nrows=1, header=None)
-    indirect_tuple = next(indirect_row.itertuples(index=False, name=None))
-    column_row = pd.read_csv(parameters_file, sep=';', skiprows=[0], nrows=1, header=None)
-    column_tuple = next(column_row.itertuples(index=False, name=None))
-    argnames = ', '.join(column_tuple[1:])
-    indirect = []
-    for i in zip(indirect_tuple, column_tuple):
-        if i[0] == 'indirect':
-            indirect.append(i[1])
+    try:
+        indirect_row = pd.read_csv(parameters_file, sep=';', nrows=1, header=None)
+        indirect_tuple = next(indirect_row.itertuples(index=False, name=None))
+        column_row = pd.read_csv(parameters_file, sep=';', skiprows=[0], nrows=1, header=None)
+        column_tuple = next(column_row.itertuples(index=False, name=None))
+        argnames = ', '.join(column_tuple[1:])
+        indirect = []
+        for i in zip(indirect_tuple, column_tuple):
+            if i[0] == 'indirect':
+                indirect.append(i[1])
 
-    df = pd.read_csv(parameters_file, sep=';', skiprows=[0], converters={'base_files': ast.literal_eval})
-    df.expected = df.expected.str.replace('\\n', '\n')
-    df.expected = df.expected.str.replace('\\"', '"')
-    ids = []
-    testdata = []
-    for index, row in df.iterrows():
-        id = str(row[:1].item())
-        td = tuple(row[1:])
-        ids.append(id)
-        testdata.append(td)
+        df = pd.read_csv(parameters_file, sep=';', skiprows=[0], converters={'base_files': ast.literal_eval})
+        df.expected = df.expected.str.replace('\\n', '\n')
+        df.expected = df.expected.str.replace('\\"', '"')
+        ids = []
+        testdata = []
+        for index, row in df.iterrows():
+            id = str(row[:1].item())
+            td = tuple(row[1:])
+            ids.append(id)
+            testdata.append(td)
 
-    return argnames, testdata, ids, indirect
+        return argnames, testdata, ids, indirect
+
+    except FileNotFoundError:
+        test_name = parameters_file.split('.')[0]
+        raise Exception(f'\nFor test: "{test_name}"\nExpected: "{parameters_file}" file with parameters\n'
+                        f'But .csv file is not found (it should be located in the same directory as .py '
+                        f'file with the test)') from None
