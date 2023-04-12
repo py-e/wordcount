1b72649367bb384825d9ee0940f85f2656f967ab
py-e <gitpye@gmail.com>
2023-04-12 21:21:56 +0300

    Merge pytest updates

--- tests/unittest_integration_tests.py
+++ tests/unittest_integration_tests.py
@@ -5,7 +5,7 @@ import glob
 import shutil
 import unittest
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import count_words
 
 
